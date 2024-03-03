e2ac9c7b5a0ebcdb4f0d6af73cd4c61218725104
AGPLCZ <62847503+AGPLCZ@users.noreply.github.com>
2024-03-03 11:23:46 +0100

    Update sms.php

--- sms/sms.php
+++ sms/sms.php
@@ -7,6 +7,7 @@ error_reporting(E_ERROR | E_WARNING);
 $payload = file_get_contents('php://input');
 $decoded = json_decode($payload, true);
 
+
 // Příprava a spuštění SQL příkazu pro vložení dat
 $sql = "INSERT INTO sms_messages (`from`, `fromName`, `text`, sent_stamp, received_stamp, sim) VALUES (?, ?, ?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
@@ -14,10 +15,9 @@ $stmt = $conn->prepare($sql);
 // Převod milisekundových Unix timestampů na formát MySQL DATETIME
 $sentStamp = date('Y-m-d H:i:s', $decoded['sentStamp'] / 1000);
 $receivedStamp = date('Y-m-d H:i:s', $decoded['receivedStamp'] / 1000);
-
-$stmt->bind_param("ssssss", $decoded['from'], $decoded['fromName'], $decoded['text'], $sentStamp, $receivedStamp, $decoded['sim']);
+$from = str_replace('+', '', $decoded['from']); // Odebrání plus z telefonního čísla
+$stmt->bind_param("ssssss", $from, $decoded['fromName'], $decoded['text'], $sentStamp, $receivedStamp, $decoded['sim']);
 $stmt->execute();
 $stmt->close();
 
 $conn->close();
-?>
